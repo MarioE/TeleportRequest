a7b7f1c0dbf4923a27c124d64a1547e23550cd81
MarioE <MarioE27@hotmail.com>
2012-08-10 21:23:38 -0400

    Yeah, put my name there.

--- TeleportRequest/Properties/AssemblyInfo.cs
+++ TeleportRequest/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TeleportRequest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("MarioE")]
 [assembly: AssemblyProduct("TeleportRequest")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © MarioE 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
